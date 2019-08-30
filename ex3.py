0ea2bf608337fbe4a17ab3fc5663f2a35addd223
m07012 <54697809+m07012@users.noreply.github.com>
2019-08-30 17:03:22 +0700

    Python Exercise
    LPTHW

---
+++ ex3.py
@@ -0,0 +1,18 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?" , 3 + 2
+
+print 7.0 / 4
+print 7 / 4.0
+print 7 % 4
